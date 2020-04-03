7a009fe4ec5f119a8a23e7b7e060401cbcc9ff38
Vladislav <61116644+ldreamer21@users.noreply.github.com>
2020-04-03 16:38:04 +0300

    Доработана 6 лабораторная работа
    +добавлены копирующий конструктор и перегрузка операции равно (=) для производного класса
    +проверена работа методов базовых классов, вызванных через производный классов
    +выполнены тесты программы в целом
    +добавлены ответы на контрольные вопросы

--- Лабораторная 6/Project/Project/Source.cpp
+++ Лабораторная 6/Project/Project/Source.cpp
@@ -31,7 +31,7 @@ Air2<T, P>::Air2()
 	max_altitude = 11.4;
 	max_spead = 521.2;
 	delete[] type;
-	type = new char[10];
+	type = new char[7];
 	strcpy(type, "public");
 }
 
@@ -200,7 +200,7 @@ void PersonalTransport::set_information() {
 
 void PersonalTransport::get_information() const {
 	cout << "Имя вашего водителя/пилота: " << drivers_name << endl;
-	cout << "Введите траты на транспорт в месяц: " << service_a_month << endl;
+	cout << "Траты на транспорт в месяц: " << service_a_month << '$' << endl;
 }
 
 PersonalTransport& PersonalTransport::operator=(const PersonalTransport& obj) {
@@ -219,9 +219,11 @@ class Plane : public Air2<int, double>, public PersonalTransport {
 public:
 	Plane();
 	Plane(const char* str, const int d, const int a, const double s, const char* name, const double price, const int capacity);
+	Plane(const Plane<T, P>& obj);
 	~Plane();
 	void input();
 	void print() const;
+	Plane<T, P>& operator=(const Plane<T, P>& obj);
 
 protected:
 	int capacity; //вместимость
@@ -240,6 +242,27 @@ Plane<T, P>::Plane(const char* str, const int d, const int a, const double s, co
 }
 
 template <class T, class P>
+Plane<T, P>::Plane(const Plane<T, P>& obj)
+{
+	if (&obj != this) {
+		//data 1st base class
+		distance_of_flight = obj.distance_of_flight;
+		max_altitude = obj.max_altitude;
+		max_spead = obj.max_spead;
+		delete[] type;
+		type = new char[strlen(obj.type) + 1];
+		strcpy(type, obj.type);
+		//data 2nd base class
+		delete[] drivers_name;
+		drivers_name = new char[strlen(obj.drivers_name) + 1];
+		strcpy(drivers_name, obj.drivers_name);
+		service_a_month = obj.service_a_month;
+		//own data
+		capacity = obj.capacity;
+	}
+}
+
+template <class T, class P>
 Plane<T, P>::~Plane()
 {
 	Air2::~Air2();
@@ -251,7 +274,7 @@ void Plane<T, P>::input()
 {
 	Air2::input();
 	PersonalTransport::set_information();
-	cout << "Введите вместимость самолёта: ";
+	cout << "Введите вместимость самолёта (кол-во человек): ";
 	cin >> capacity;
 }
 
@@ -260,7 +283,28 @@ void Plane<T, P>::print() const
 {
 	Air2::print();
 	PersonalTransport::get_information();
-	cout << "Вместимость самолёта: " << capacity << endl;
+	cout << "Вместимость самолёта: " << capacity << " человек" << endl;
+}
+
+template <class T, class P>
+Plane<T, P>& Plane<T, P>::operator=(const Plane<T, P>& obj)
+{
+	//data 1st base class
+	distance_of_flight = obj.distance_of_flight;
+	max_altitude = obj.max_altitude;
+	max_spead = obj.max_spead;
+	delete[] type;
+	type = new char[strlen(obj.type) + 1];
+	strcpy(type, obj.type);
+	//data 2nd base class
+	delete[] drivers_name;
+	drivers_name = new char[strlen(obj.drivers_name) + 1];
+	strcpy(drivers_name, obj.drivers_name);
+	service_a_month = obj.service_a_month;
+	//own data
+	capacity = obj.capacity;
+
+	return *this;
 }
 
 
@@ -274,57 +318,104 @@ int main() {
 
 	cout << "Введите информацию о первом самолёте:" << endl;
 	my_plane1.input();
-	cout << endl << "Введите информацию о втором самолёте:" << endl;
-	my_plane2.input();
+	my_plane2 = my_plane1;
 
-	cout << endl << "Информация о первом самолёте: " << endl;
+	Plane<int, double> my_plane3(my_plane2);
+
+	cout << endl << "Информация о первом самолёте:" << endl;
 	my_plane1.print();
-	cout << endl << "Информация о втором самолёте: " << endl;
+	cout << endl << "Информация о втором самолёте (копия первого с помощью оператора =):" << endl;
 	my_plane2.print();
+	cout << endl << "Информация о третьем самолёте (копия второго с помощью копирующего конструктора):" << endl;
+	my_plane3.print();
+	//вызов методов базового класса
+	cout << endl << "Изменение информации о третьем самолёте:" << endl;
+	my_plane3.Air2::input();
+	my_plane3.set_information();	//можно без полного имени - PersonalTransport::set_information()
+	cout << endl << "Новая информация по третьему самолёту:" << endl;
+	my_plane3.Air2::print();
+	my_plane3.get_information();
+
 	cout << endl;
+
 	//------------------------Plane<double, int>----------------------------
 
-	Plane<double, int> my_plane3, my_plane4;
+	Plane<double, int> my_plane4, my_plane5;
 
-	cout << "Введите информацию о третьем самолёте:" << endl;
-	my_plane3.input();
-	cout << endl << "Введите информацию о четвертом самолёте:" << endl;
+	cout << "Введите информацию о четвертом самолёте:" << endl;
 	my_plane4.input();
+	my_plane5 = my_plane4;
 
-	cout << endl << "Информация о третьем самолёте: " << endl;
-	my_plane3.print();
-	cout << endl << "Информация о четвертом самолёте: " << endl;
+	Plane<double, int> my_plane6(my_plane5);
+
+	cout << endl << "Информация о четвертом самолёте:" << endl;
 	my_plane4.print();
+	cout << endl << "Информация о пятом самолёте (копия четвертого с помощью оператора =):" << endl;
+	my_plane5.print();
+	cout << endl << "Информация о шестом самолёте (копия пятого с помощью копирующего конструктора):" << endl;
+	my_plane6.print();
+	//вызов методов базового класса
+	cout << endl << "Изменение информации о шестом самолете:" << endl;
+	my_plane6.Air2::input();
+	my_plane6.set_information();
+	cout << endl << "Новая информация о шестом самолете:" << endl;
+	my_plane6.Air2::print();
+	my_plane6.get_information();
+
 	cout << endl;
 
 	//------------------------Plane<double, double>-------------------------
 
-	Plane<double, double> my_plane5, my_plane6;
+	Plane<double, double> my_plane7, my_plane8;
 
-	cout << "Введите информацию о пятом самолёте:" << endl;
-	my_plane5.input();
-	cout << endl << "Введите информацию о шестом самолёте:" << endl;
-	my_plane6.input();
+	cout << "Введите информацию о седьмом самолёте:" << endl;
+	my_plane7.input();
+	my_plane8 = my_plane7;
 
-	cout << endl << "Информация о пятом самолёте: " << endl;
-	my_plane5.print();
-	cout << endl << "Информация о шестом самолёте: " << endl;
-	my_plane6.print();
+	Plane<double, double> my_plane9(my_plane8);
+
+	cout << endl << "Информация о седьмом самолёте:" << endl;
+	my_plane7.print();
+	cout << endl << "Информация о восьмом самолёте (копия седьмого с помощью оператора =):" << endl;
+	my_plane8.print();
+	cout << endl << "Информация о девятом самолёте (копия восьмого с помощью копирующего конструктора):" << endl;
+	my_plane9.print();
+	//вызов методов базового класса
+	cout << endl << "Изменение информации о девятом самолете:" << endl;
+	my_plane9.Air2::input();
+	my_plane9.set_information();
+	cout << endl << "Новая информация о девятом самолете:" << endl;
+	my_plane9.Air2::print();
+	my_plane9.get_information();
+
 	cout << endl;
 
 	//------------------------Plane<int, int>-------------------------------
 
-	Plane<int, int> my_plane7, my_plane8;
+	Plane<int, int> my_plane10, my_plane11;
 
-	cout << "Введите информацию о седьмом самолёте:" << endl;
-	my_plane7.input();
-	cout << endl << "Введите информацию о восьмом самолёте:" << endl;
-	my_plane8.input();
+	cout << "Введите информацию о десятом самолёте:" << endl;
+	my_plane10.input();
+	my_plane11 = my_plane10;
 
-	cout << endl << "Информация о седьмом самолёте: " << endl;
-	my_plane7.print();
-	cout << endl << "Информация о восьмом самолёте: " << endl;
-	my_plane8.print();
+	Plane<int, int> my_plane12(my_plane11);
+
+	cout << endl << "Информация о десятом самолёте:" << endl;
+	my_plane10.print();
+	cout << endl << "Информация о одиннадцитом самолёте (копия десятого с помощью оператора =):" << endl;
+	my_plane11.print();
+	cout << endl << "Информация о двенадцатом самолёте (копия одиннадцатого с помощью копирующего конструктора):" << endl;
+	my_plane12.print();
+	//вызов методов базового класса
+	cout << endl << "Изменение информации о двенадцатом самолёте:" << endl;
+	my_plane12.Air2::input();
+	my_plane12.set_information();
+	cout << endl << "Новая информация о двенадцатом самолёте:" << endl;
+	my_plane12.Air2::print();
+	my_plane12.get_information();
+
+	cout << endl << "Информация о двенадцатом самолёте (после преобразования):" << endl;
+	my_plane12.print();
 
 	system("pause");
 	return 0;
