f43c8961435996d64311b65d1f43cc19e9ad522b
Vladislav <61116644+ldreamer21@users.noreply.github.com>
2020-04-09 20:47:04 +0300

    Лабораторная 7
    Добавлен отчет, где представлен код программы, а также ответы на контрольные вопросы.

--- Лабораторная 7/Project/Project/Source.cpp
+++ Лабораторная 7/Project/Project/Source.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <stdio.h>
+#include <new>
 using namespace std;
+
+class bad_value : public exception {};
 
 template <class T, class P = int>
 class Air2 {
@@ -31,7 +34,7 @@ Air2<T, P>::Air2()
 	max_altitude = 11.4;
 	max_spead = 521.2;
 	delete[] type;
-	type = new char[10];
+	type = new char[7];
 	strcpy(type, "public");
 }
 
@@ -52,8 +55,11 @@ template <class T, class P>
 Air2<T, P>::Air2(const char* str, const T d, const T a, const P s)
 {
 	distance_of_flight = d;
+	if (distance_of_flight < 0) throw bad_value();
 	max_altitude = a;
+	if (max_altitude < 0) throw bad_value();
 	max_spead = s;
+	if (max_spead < 0) throw bad_value();
 	delete[] type;
 	type = new char[strlen(str) + 1];
 	strcpy(type, str);
@@ -72,10 +78,13 @@ void Air2<T, P>::input()
 	cin >> type;
 	cout << "Введите дистанцию полёта (км): ";
 	cin >> distance_of_flight;
+	if (distance_of_flight < 0) throw bad_value();
 	cout << "Введите максимальную высоту полёта (км): ";
 	cin >> max_altitude;
+	if (max_altitude < 0) throw bad_value();
 	cout << "Введите максимальную скорость (км/ч): ";
 	cin >> max_spead;
+	if (max_spead < 0) throw bad_value();
 }
 
 template <class T, class P>
@@ -170,13 +179,14 @@ PersonalTransport::PersonalTransport() {
 	delete[] drivers_name;
 	drivers_name = new char[10];
 	strcpy(drivers_name, "Alex");
-	service_a_month = 1000;
+	service_a_month = 10000;
 }
 
 PersonalTransport::PersonalTransport(const char* name, const double price) {
 	delete[] drivers_name;
 	drivers_name = new char[strlen(name) + 1];
 	strcpy(drivers_name, name);
+	if (price < 0) throw bad_value();
 	service_a_month = price;
 }
 
@@ -196,11 +206,12 @@ void PersonalTransport::set_information() {
 	cin >> drivers_name;
 	cout << "Введите траты на транспорт в месяц: ";
 	cin >> service_a_month;
+	if (service_a_month < 0) throw bad_value();
 }
 
 void PersonalTransport::get_information() const {
 	cout << "Имя вашего водителя/пилота: " << drivers_name << endl;
-	cout << "Введите траты на транспорт в месяц: " << service_a_month << endl;
+	cout << "Траты на транспорт в месяц: " << service_a_month << '$' << endl;
 }
 
 PersonalTransport& PersonalTransport::operator=(const PersonalTransport& obj) {
@@ -219,9 +230,11 @@ class Plane : public Air2<int, double>, public PersonalTransport {
 public:
 	Plane();
 	Plane(const char* str, const int d, const int a, const double s, const char* name, const double price, const int capacity);
+	Plane(const Plane<T, P>& obj);
 	~Plane();
 	void input();
 	void print() const;
+	Plane<T, P>& operator=(const Plane<T, P>& obj);
 
 protected:
 	int capacity; //вместимость
@@ -236,7 +249,29 @@ Plane<T, P>::Plane() : Air2(), PersonalTransport()
 template <class T, class P>
 Plane<T, P>::Plane(const char* str, const int d, const int a, const double s, const char* name, const double price, const int capacity) : Air2(str, d, a, s), PersonalTransport(name, price)
 {
+	if (capacity < 0) throw bad_value();
 	this->capacity = capacity;
+}
+
+template <class T, class P>
+Plane<T, P>::Plane(const Plane<T, P>& obj)
+{
+	if (&obj != this) {
+		//data 1st base class
+		distance_of_flight = obj.distance_of_flight;
+		max_altitude = obj.max_altitude;
+		max_spead = obj.max_spead;
+		delete[] type;
+		type = new char[strlen(obj.type) + 1];
+		strcpy(type, obj.type);
+		//data 2nd base class
+		delete[] drivers_name;
+		drivers_name = new char[strlen(obj.drivers_name) + 1];
+		strcpy(drivers_name, obj.drivers_name);
+		service_a_month = obj.service_a_month;
+		//own data
+		capacity = obj.capacity;
+	}
 }
 
 template <class T, class P>
@@ -251,8 +286,9 @@ void Plane<T, P>::input()
 {
 	Air2::input();
 	PersonalTransport::set_information();
-	cout << "Введите вместимость самолёта: ";
+	cout << "Введите вместимость самолёта (кол-во человек): ";
 	cin >> capacity;
+	if (capacity < 0) throw bad_value();
 }
 
 template <class T, class P>
@@ -260,72 +296,194 @@ void Plane<T, P>::print() const
 {
 	Air2::print();
 	PersonalTransport::get_information();
-	cout << "Вместимость самолёта: " << capacity << endl;
+	cout << "Вместимость самолёта: " << capacity << " человек" << endl;
 }
 
+template <class T, class P>
+Plane<T, P>& Plane<T, P>::operator=(const Plane<T, P>& obj)
+{
+	//data 1st base class
+	distance_of_flight = obj.distance_of_flight;
+	max_altitude = obj.max_altitude;
+	max_spead = obj.max_spead;
+	delete[] type;
+	type = new char[strlen(obj.type) + 1];
+	strcpy(type, obj.type);
+	//data 2nd base class
+	delete[] drivers_name;
+	drivers_name = new char[strlen(obj.drivers_name) + 1];
+	strcpy(drivers_name, obj.drivers_name);
+	service_a_month = obj.service_a_month;
+	//own data
+	capacity = obj.capacity;
+
+	return *this;
+}
 
 int main() {
 
 	setlocale(LC_ALL, "Russian");
-
+	bool exception;
 	//------------------------Plane<int, double>---------------------------
 
-	Plane<int, double> my_plane1, my_plane2;
+	do {
+		try
+		{
+			exception = false;
+			Plane<int, double> my_plane1, my_plane2;
 
-	cout << "Введите информацию о первом самолёте:" << endl;
-	my_plane1.input();
-	cout << endl << "Введите информацию о втором самолёте:" << endl;
-	my_plane2.input();
+			cout << "Введите информацию о первом самолёте:" << endl;
+			my_plane1.input();
+			my_plane2 = my_plane1;
 
-	cout << endl << "Информация о первом самолёте: " << endl;
-	my_plane1.print();
-	cout << endl << "Информация о втором самолёте: " << endl;
-	my_plane2.print();
-	cout << endl;
+			Plane<int, double> my_plane3(my_plane2);
+
+			cout << endl << "Информация о первом самолёте:" << endl;
+			my_plane1.print();
+			cout << endl << "Информация о втором самолёте (копия первого с помощью оператора =):" << endl;
+			my_plane2.print();
+			cout << endl << "Информация о третьем самолёте (копия второго с помощью копирующего конструктора):" << endl;
+			my_plane3.print();
+			//вызов методов базового класса
+			cout << endl << "Изменение информации о третьем самолёте:" << endl;
+			my_plane3.Air2::input();
+			my_plane3.set_information();	//можно без полного имени - PersonalTransport::set_information()
+			cout << endl << "Новая информация по третьему самолёту:" << endl;
+			my_plane3.Air2::print();
+			my_plane3.get_information();
+			cout << endl;
+		}
+		catch (bad_alloc& e) {
+			cout << "Память под строку не выделена. Введите ещё раз." << endl;
+			exception = true;
+		}
+		catch (bad_value& e) {
+			cout << "Введено несоответствующее значение. Введите ещё раз." << endl;
+			exception = true;
+		}
+	} while (exception == true);
+	
 	//------------------------Plane<double, int>----------------------------
 
-	Plane<double, int> my_plane3, my_plane4;
+	do {
+		try
+		{
+			exception = false;
+			Plane<double, int> my_plane4, my_plane5;
 
-	cout << "Введите информацию о третьем самолёте:" << endl;
-	my_plane3.input();
-	cout << endl << "Введите информацию о четвертом самолёте:" << endl;
-	my_plane4.input();
+			cout << "Введите информацию о четвертом самолёте:" << endl;
+			my_plane4.input();
+			my_plane5 = my_plane4;
 
-	cout << endl << "Информация о третьем самолёте: " << endl;
-	my_plane3.print();
-	cout << endl << "Информация о четвертом самолёте: " << endl;
-	my_plane4.print();
-	cout << endl;
+			Plane<double, int> my_plane6(my_plane5);
+
+			cout << endl << "Информация о четвертом самолёте:" << endl;
+			my_plane4.print();
+			cout << endl << "Информация о пятом самолёте (копия четвертого с помощью оператора =):" << endl;
+			my_plane5.print();
+			cout << endl << "Информация о шестом самолёте (копия пятого с помощью копирующего конструктора):" << endl;
+			my_plane6.print();
+			//вызов методов базового класса
+			cout << endl << "Изменение информации о шестом самолете:" << endl;
+			my_plane6.Air2::input();
+			my_plane6.set_information();
+			cout << endl << "Новая информация о шестом самолете:" << endl;
+			my_plane6.Air2::print();
+			my_plane6.get_information();
+
+			cout << endl;
+		}
+		catch (bad_alloc& e) {
+			cout << "Память под строку не выделена. Введите ещё раз." << endl;
+			exception = true;
+		}
+		catch (bad_value& e) {
+			cout << "Введено несоответствующее значение. Введите ещё раз." << endl;
+			exception = true;
+		}
+	} while (exception == true);
 
 	//------------------------Plane<double, double>-------------------------
 
-	Plane<double, double> my_plane5, my_plane6;
+	do {
+		try
+		{
+			exception = false;
+			Plane<double, double> my_plane7, my_plane8;
 
-	cout << "Введите информацию о пятом самолёте:" << endl;
-	my_plane5.input();
-	cout << endl << "Введите информацию о шестом самолёте:" << endl;
-	my_plane6.input();
+			cout << "Введите информацию о седьмом самолёте:" << endl;
+			my_plane7.input();
+			my_plane8 = my_plane7;
 
-	cout << endl << "Информация о пятом самолёте: " << endl;
-	my_plane5.print();
-	cout << endl << "Информация о шестом самолёте: " << endl;
-	my_plane6.print();
-	cout << endl;
+			Plane<double, double> my_plane9(my_plane8);
+
+			cout << endl << "Информация о седьмом самолёте:" << endl;
+			my_plane7.print();
+			cout << endl << "Информация о восьмом самолёте (копия седьмого с помощью оператора =):" << endl;
+			my_plane8.print();
+			cout << endl << "Информация о девятом самолёте (копия восьмого с помощью копирующего конструктора):" << endl;
+			my_plane9.print();
+			//вызов методов базового класса
+			cout << endl << "Изменение информации о девятом самолете:" << endl;
+			my_plane9.Air2::input();
+			my_plane9.set_information();
+			cout << endl << "Новая информация о девятом самолете:" << endl;
+			my_plane9.Air2::print();
+			my_plane9.get_information();
+
+			cout << endl;
+		}
+		catch (bad_alloc& e) {
+			cout << "Память под строку не выделена. Введите ещё раз." << endl;
+			exception = true;
+		}
+		catch (bad_value& e) {
+			cout << "Введено несоответствующее значение. Введите ещё раз." << endl;
+			exception = true;
+		}
+	} while (exception == true);
 
 	//------------------------Plane<int, int>-------------------------------
 
-	Plane<int, int> my_plane7, my_plane8;
+	do {
+		try
+		{
+			exception = false;
+			Plane<int, int> my_plane10, my_plane11;
 
-	cout << "Введите информацию о седьмом самолёте:" << endl;
-	my_plane7.input();
-	cout << endl << "Введите информацию о восьмом самолёте:" << endl;
-	my_plane8.input();
+			cout << "Введите информацию о десятом самолёте:" << endl;
+			my_plane10.input();
+			my_plane11 = my_plane10;
 
-	cout << endl << "Информация о седьмом самолёте: " << endl;
-	my_plane7.print();
-	cout << endl << "Информация о восьмом самолёте: " << endl;
-	my_plane8.print();
+			Plane<int, int> my_plane12(my_plane11);
 
-	system("pause");
+			cout << endl << "Информация о десятом самолёте:" << endl;
+			my_plane10.print();
+			cout << endl << "Информация о одиннадцитом самолёте (копия десятого с помощью оператора =):" << endl;
+			my_plane11.print();
+			cout << endl << "Информация о двенадцатом самолёте (копия одиннадцатого с помощью копирующего конструктора):" << endl;
+			my_plane12.print();
+			//вызов методов базового класса
+			cout << endl << "Изменение информации о двенадцатом самолёте:" << endl;
+			my_plane12.Air2::input();
+			my_plane12.set_information();
+			cout << endl << "Новая информация о двенадцатом самолёте:" << endl;
+			my_plane12.Air2::print();
+			my_plane12.get_information();
+
+			cout << endl << "Информация о двенадцатом самолёте (после преобразования):" << endl;
+			my_plane12.print();
+		}
+		catch (bad_alloc& e) {
+			cout << "Память под строку не выделена. Введите ещё раз." << endl;
+			exception = true;
+		}
+		catch (bad_value& e) {
+			cout << "Введено несоответствующее значение. Введите ещё раз." << endl;
+			exception = true;
+		}
+	} while (exception == true);
+	
+	std::system("pause");
 	return 0;
 }
